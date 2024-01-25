bf35e2e02dcca8017abd205dd1ad4ea12db2e829
dana096 <lovejiwon11@naver.com>
2024-01-25 17:37:12 +0900

    modify index.js

--- index.js
+++ index.js
@@ -10,12 +10,6 @@ console.log(`
 `);
 
 
-// process.argv를 사용하여 발표자 목록을 입력(최소 2명의 발표자가 필요함)
-// let args = process.argv.slice(2);
-// let length = args.length;
-
-// Math.random()을 사용하여 랜덤한 발표자를 선택
-// console.log('발표자 : ',args[Math.floor(Math.random() * length)]);
 
 class PresenterList {
 	constructor(presenters) {
