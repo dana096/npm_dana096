bb9463db8be34061b84d044bc140ca926d2db88d
dana096 <lovejiwon11@naver.com>
2024-01-25 17:34:47 +0900

    요구사항 작성하기

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
 #!/usr/bin/env node
 
-const asciiArt = '
+console.log(`
 ┌───────────────┐
        200 OK     
 └───────────────┘
 　　ᕱ ᕱ ||
   ( ･ω･ ||
 　 /　つΦ
-';
+`);
 
-console.log(asciiArt);
 
-// process.argv를 사용하여 발표자 목록을 입력
-const args = process.argv.slice(2);
-
-// 최소 두 명의 발표자가 필요함
-if (args.length < 2) {
-  console.error('적어도 두 명의 발표자가 필요합니다.');
-  process.exit(1);
-}
+// process.argv를 사용하여 발표자 목록을 입력(최소 2명의 발표자가 필요함)
+// let args = process.argv.slice(2);
+// let length = args.length;
 
 // Math.random()을 사용하여 랜덤한 발표자를 선택
-const randomIndex = Math.floor(Math.random() * args.length);
-const selectedPresenter = args[randomIndex];
+// console.log('발표자 : ',args[Math.floor(Math.random() * length)]);
 
-// 선택된 발표자를 출력
-console.log(`발표자: ${selectedPresenter}`);
+class PresenterList {
+	constructor(presenters) {
+		this.presenters = presenters;
+	}
+	pickedRandomPresenter() {
+		const length = this.presenters.length;
+		const selectedPresenter = this.presenters[Math.floor(Math.random() * length)];
+		console.log('발표자 : ', selectedPresenter);
+	}
+}
+
+class ExtendsPickedRandomPresenter extends PresenterList {
+	constructor(presenters) {
+		super (presenters);
+	}
+}
+
+const args = process.argv.slice(2);
+const extendsPickedRandomPresenter = new ExtendsPickedRandomPresenter(args);
+extendsPickedRandomPresenter.pickedRandomPresenter();
+
+
